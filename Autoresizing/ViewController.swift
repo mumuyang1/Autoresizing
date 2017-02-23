0ea238ef4e614e470bc0f40b5ef56a93cbc13278
Li Yanzi <yanzi.li@rea-group.com>
2017-02-23 16:35:11 +1100

    implement banner using autoresizing

--- Autoresizing/ViewController.swift
+++ Autoresizing/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var purpleView: UIView!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    let blueView = UIView(frame: CGRect(x:0, y:200, width: view.bounds.width, height:100))
+    blueView.backgroundColor = UIColor.blue
+    blueView.autoresizingMask = [.flexibleTopMargin, .flexibleWidth, .flexibleBottomMargin, .flexibleHeight]
+    view.addSubview(blueView)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    // if view is added in storyboard then set autoresizing in sb
+    // if view is added in code then set autoresizing in code
+    // it's a bad practise that set autoresizing in code while view is added by storyoard
+    purpleView.frame = CGRect(x:0, y:purpleView.frame.minY, width: view.bounds.width, height: purpleView.bounds.height )
+    purpleView.autoresizingMask = [.flexibleTopMargin, .flexibleWidth, .flexibleBottomMargin, .flexibleHeight]
+    
+    print(purpleView.bounds.minY)
+    
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
